8f1aab442bf5ead84063648fb4f13fc6beaa638b
xia-aa <xia-aa@outlook.com>
2025-03-23 04:51:04 +0800

    alg-enum

--- .vscode/1.py
+++ .vscode/1.py
@@ -1,5 +1,5 @@
 import sys
 input = lambda: sys.stdin.readline().strip()
 n, m = map(int, input().split())
-matrix = [list(map(int, input().split())) for _ in range(n)]
-print(matrix)
+zero_matrix3 = [[0]*m]*n
+print(zero_matrix3)
