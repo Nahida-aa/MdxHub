fdb4bbee16ea1efffa95d680f776ea08a7f15eb6
xia-aa <xia-aa@outlook.com>
2025-03-23 01:37:25 +0800

    alg-enum

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next'
 import siteMetadata from 'src/data/siteMetadata'
 
+export const dynamic = "force-static";
+export const revalidate = 60; // Set revalidation interval or remove if not needed
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
