f92f8f6ab87ce33a85a4a940806a9937107b0051
xia-aa <xia-aa@outlook.com>
2025-03-23 19:43:54 +0800

    index

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,6 +2,9 @@ import { MetadataRoute } from 'next'
 // import { allBlogs } from 'contentlayer/generated'
 import siteMetadata from 'src/data/siteMetadata'
 
+export const dynamic = process.env.EXPORT ? 'force-static' : undefined
+export const revalidate = process.env.EXPORT ? 60 : undefined; // revalidate every 60 seconds
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = siteMetadata.siteUrl
 
